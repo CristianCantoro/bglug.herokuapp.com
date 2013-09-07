2f8b2604751f201b0ebef5950a66126226996b2c
Emiliano Vavassori <syntaxerrormmm@gmail.com>
2013-09-07 05:29:01 +0200

    Implementte pagine separate, almeno quella inizia.

--- Gemfile
+++ Gemfile
@@ -3,5 +3,20 @@ source "https://rubygems.org"
 
 ruby "2.0.0"
 
-# gem "rails"
-gem 'sinatra', '1.4.3' 
+group :development do
+  gem 'sinatra', '1.4.3'
+  gem 'haml', '4.0.3'
+  gem 'kramdown', '1.2.0'
+  gem 'kramdown-haml', '0.0.3'
+  gem 'sinatra-captcha', '0.1.0.0'
+  gem 'thin', '1.5.1'
+  gem 'sass', '3.2.10'
+end
+
+group :production do
+  gem 'sinatra', '1.4.3'
+  gem 'haml', '4.0.3'
+  gem 'kramdown', '1.2.0'
+  gem 'kramdown-haml', '0.0.3'
+  gem 'sinatra-captcha', '0.1.0.0'
+end
--- web.rb
+++ web.rb
@@ -1,5 +1,32 @@
 require 'sinatra'
+require 'sinatra/captcha'
+require 'haml'
+require 'kramdown'
+require 'kramdown-haml'
 
 get '/' do
-  "Work in progress - BGlug.it"
+  redirect '/ld/2013/'
 end
+
+get '/ld/2013/?' do
+  haml 'ld/2013/index'.to_sym, :format => :html5, :layout => :linuxday
+end
+
+get '/ld/2013/segui' do
+  haml 'ld/2013/segui'.to_sym, :format => :html5, :layout => :linuxday
+end
+
+not_found do
+  haml :notfound
+end
+
+__END__
+
+@@notfound
+!!!
+%html(xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en")
+  %head
+    %meta(charset="UTF-8")
+    %title 404 - Not Found
+  %body
+    %p{style="margin: auto; text-align: center; font-size: 40px"} 404 - Not Found.
