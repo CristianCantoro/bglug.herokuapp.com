d876316da62e75f2effa359ca3ad37424ae993ce
Emiliano Vavassori <syntaxerrormmm@gmail.com>
2013-09-08 05:56:21 +0200

    Versione funzionante di tutti i form.

--- Gemfile
+++ Gemfile
@@ -9,6 +9,8 @@ group :development do
   gem 'kramdown', '1.2.0'
   gem 'kramdown-haml', '0.0.3'
   gem 'sinatra-captcha', '0.1.0.0'
+  gem 'google_drive', '0.3.6'
+  gem 'zip', '2.0.2'
   gem 'thin', '1.5.1'
   gem 'sass', '3.2.10'
 end
@@ -19,4 +21,6 @@ group :production do
   gem 'kramdown', '1.2.0'
   gem 'kramdown-haml', '0.0.3'
   gem 'sinatra-captcha', '0.1.0.0'
+  gem 'google_drive', '0.3.6'
+  gem 'zip', '2.0.2'
 end
--- web.rb
+++ web.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require 'haml'
 require 'kramdown'
 require 'kramdown-haml'
+require 'google_drive'
 
 get '/' do
   redirect '/ld/2013/'
@@ -13,6 +14,60 @@ end
 
 get '/ld/2013/segui' do
   haml 'ld/2013/segui'.to_sym, :format => :html5, :layout => :linuxday
+end
+
+get '/ld/2013/presenta' do
+  haml 'ld/2013/presenta'.to_sym, :format => :html5, :layout => :linuxday
+end
+
+get '/ld/2013/installa' do
+  haml 'ld/2013/installa'.to_sym, :format => :html5, :layout => :linuxday
+end
+
+post '/ld/2013/conferma' do
+  if params[:segui] and params[:submit] == 'Conferma'
+    session = GoogleDrive.login('bglug.herokuapp@gmail.com', 'kill4win')
+    oo = session.spreadsheet_by_key('0AiGfqla6ISdEdEJHYnNJYVEyS09ER2ctU203eVJVc0E').worksheets[1]
+    row = oo.num_rows + 1
+
+    oo[row, 1] = params[:nome]
+    oo[row, 2] = params[:email]
+    oo[row, 3] = params[:accompagnatore]
+    oo[row, 4] = params[:accompagnati]
+    oo[row, 5] = params[:note]
+    oo.synchronize()
+
+    haml 'ld/2013/conferma_segui'.to_sym, :layout => :linuxday, :format => :html5, :locals => params
+  elsif params[:presenta] and params[:submit] == 'Conferma'
+    session = GoogleDrive.login('bglug.herokuapp@gmail.com', 'kill4win')
+    oo = session.spreadsheet_by_key('0AiGfqla6ISdEdEJHYnNJYVEyS09ER2ctU203eVJVc0E').worksheets[0]
+    row = oo.num_rows + 1
+
+    oo[row, 1] = params[:nome]
+    oo[row, 2] = params[:email]
+    oo[row, 3] = params[:titolo]
+    oo[row, 4] = params[:sessione]
+    oo[row, 5] = params[:formato]
+    oo[row, 6] = params[:abstract]
+    oo.synchronize()
+
+    haml 'ld/2013/conferma_presenta'.to_sym, :format => :html5, :layout => :linuxday, :locals => params
+  elsif params[:installa] and params[:submit] == 'Conferma'
+    session = GoogleDrive.login('bglug.herokuapp@gmail.com', 'kill4win')
+    oo = session.spreadsheet_by_key('0AiGfqla6ISdEdEJHYnNJYVEyS09ER2ctU203eVJVc0E').worksheets[3]
+    row = oo.num_rows + 1
+
+    oo[row, 1] = params[:nome]
+    oo[row, 2] = params[:email]
+    oo[row, 3] = params[:tipo]
+    oo[row, 4] = params[:cpu]
+    oo[row, 5] = params[:ram]
+    oo[row, 6] = params[:flavor]
+    oo[row, 7] = params[:note]
+    oo.synchronize()
+
+    haml 'ld/2013/conferma_installa'.to_sym, :format => :html5, :layout => :linuxday, :locals => params
+  end
 end
 
 not_found do
