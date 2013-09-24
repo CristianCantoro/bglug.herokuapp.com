3cf060a0ed79bcca527683651c125f8797969ee3
CristianCantoro <kikkocristian@gmail.com>
2013-09-25 01:20:48 +0200

    Add parking data

---
+++ public/js/parkings_geojson.js
@@ -0,0 +1,267 @@
+var parkings = {
+  "type": "FeatureCollection",
+  "generator": "overpass-turbo",
+  "copyright": "The data included in this document is from www.openstreetmap.org. The data is made available under ODbL.",
+  "timestamp": "2013-09-24T22:53:02Z",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "way/38233828",
+        "amenity": "parking",
+        "parking": "surface"
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [
+          [
+            [
+              9.6283795,
+              45.6935706
+            ],
+            [
+              9.6286291,
+              45.6935679
+            ],
+            [
+              9.6286215,
+              45.6932284
+            ],
+            [
+              9.6283719,
+              45.6932311
+            ],
+            [
+              9.6283795,
+              45.6935706
+            ]
+          ]
+        ]
+      },
+      "id": "way/38233828"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "way/61712445",
+        "amenity": "parking",
+        "fee": "no",
+        "parking": "surface"
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [
+          [
+            [
+              9.6308297,
+              45.6951601
+            ],
+            [
+              9.630788,
+              45.6945322
+            ],
+            [
+              9.6309815,
+              45.6945163
+            ],
+            [
+              9.6311067,
+              45.6951389
+            ],
+            [
+              9.6308297,
+              45.6951601
+            ]
+          ]
+        ]
+      },
+      "id": "way/61712445"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "way/81083018",
+        "amenity": "parking",
+        "name": "Parcheggio Polaresco",
+        "parking": "surface"
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [
+          [
+            [
+              9.6343469,
+              45.6911106
+            ],
+            [
+              9.6344749,
+              45.6909781
+            ],
+            [
+              9.6348876,
+              45.6911726
+            ],
+            [
+              9.6347596,
+              45.6913051
+            ],
+            [
+              9.6343469,
+              45.6911106
+            ]
+          ]
+        ]
+      },
+      "id": "way/81083018"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "way/81083029",
+        "amenity": "parking",
+        "parking": "surface"
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [
+          [
+            [
+              9.640832,
+              45.6913985
+            ],
+            [
+              9.6408751,
+              45.6912815
+            ],
+            [
+              9.6412909,
+              45.6913498
+            ],
+            [
+              9.6412614,
+              45.6914695
+            ],
+            [
+              9.640832,
+              45.6913985
+            ]
+          ]
+        ]
+      },
+      "id": "way/81083029"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "way/81083045",
+        "amenity": "parking",
+        "parking": "surface"
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [
+          [
+            [
+              9.6397376,
+              45.6912695
+            ],
+            [
+              9.6397936,
+              45.6910879
+            ],
+            [
+              9.6405567,
+              45.691208
+            ],
+            [
+              9.6405369,
+              45.691254
+            ],
+            [
+              9.6407062,
+              45.6912691
+            ],
+            [
+              9.6406699,
+              45.6912999
+            ],
+            [
+              9.640622,
+              45.6913799
+            ],
+            [
+              9.6400847,
+              45.6912797
+            ],
+            [
+              9.6400686,
+              45.6913259
+            ],
+            [
+              9.6397376,
+              45.6912695
+            ]
+          ]
+        ]
+      },
+      "id": "way/81083045"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "way/99871065",
+        "amenity": "parking",
+        "name": "Parcheggio scuole",
+        "parking": "surface"
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [
+          [
+            [
+              9.6350339,
+              45.6921982
+            ],
+            [
+              9.6351832,
+              45.6920544
+            ],
+            [
+              9.6348664,
+              45.6918805
+            ],
+            [
+              9.6348099,
+              45.6919453
+            ],
+            [
+              9.6349411,
+              45.6921966
+            ],
+            [
+              9.6350339,
+              45.6921982
+            ]
+          ]
+        ]
+      },
+      "id": "way/99871065"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/1110336870",
+        "amenity": "parking",
+        "name": "Parcheggio C6"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          9.6310509,
+          45.6913203
+        ]
+      },
+      "id": "node/1110336870"
+    }
+  ]
+}
